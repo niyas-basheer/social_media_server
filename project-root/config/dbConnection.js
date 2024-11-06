bd7679a590ba79ba7a12467fe46f06f4bb384cd6
Niyas Basheer <niyasbasheer@Niyass-MacBook-Air.local>
2024-11-06 10:57:35 +0530

    Remove unwanted file or directory from Git

--- project-root/config/dbConnection.js
+++
@@ -1,7 +0,0 @@
-// MongoDB Connection
-connectDB = mongoose.connect("YOUR_MONGODB_CONNECTION_STRING", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
