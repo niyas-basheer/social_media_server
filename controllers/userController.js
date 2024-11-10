632761456a0876251ccb337756dd5b8bd02346bb
Rithick <rithick.panoor574@gmail.com>
2024-11-10 19:20:40 +0530

    feat:resend otp api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,12 +73,9 @@ const requestOtp = async (req, res) => {
   try {
     const { phone } = req.body;
 
-    let user = await User.findOne({ phoneNumber: phone });
+    const otpEntry = await OtpModel.findOne({ phone });
 
-    if (user)
-      return res.status(400).json({
-        msg: "User with the same number already exists! Please check",
-      });
+    if (otpEntry) return res.status(403).json("we already send a otp");
 
     const otpValue = generateOtp();
 
@@ -99,32 +96,18 @@ const requestOtp = async (req, res) => {
 
 // Controller for logging in with OTP
 const loginWithOtp = async (req, res) => {
-  console.log("Request Body:", req.body); // Log the request body
   const { phone, otp } = req.body;
 
   try {
     const otpEntry = await OtpModel.findOne({ phone });
-    console.log("otpEntry:",otpEntry);
-    if (!otpEntry)
 
-      return res.status(404).json("OTP not found for this phone number.");
+    if (!otpEntry) return res.status(404).json("OTP Expired");
 
-    if (otpEntry.otp !== otp) {
-      return res.status(400).json("Invalid OTP.");
-    }
+    if (otpEntry.otp !== otp) return res.status(400).json("Invalid OTP");
 
-    if (Date.now() > otpEntry.otpExpiry) {
-      return res.status(400).json("OTP Expired");
-    }
-    
-    
-    console.log("Request Body:1");
-    await otpEntry.save();
-    console.log("Request Body:2");
     let user = await User.findOne({ phoneNumber: phone });
 
     if (!user) {
-      console.log("Request Body:3");
       user = new User({
         username: `user_${phone}`,
         phoneNumber: phone,
@@ -132,16 +115,43 @@ const loginWithOtp = async (req, res) => {
       });
 
       await user.save();
-    };
-    console.log("Request Body:4");
+    }
+
     user.isOnline = true;
     await user.save();
 
     res.json({ message: "Login successful", user });
-    console.log("Request Body:5");
   } catch (err) {
     console.error("Error occurred:", err);
     res.status(500).json("Error occurred: " + err.message);
+  }
+};
+
+const resendOtp = async (req, res) => {
+  try {
+    const { phone } = req.body;
+
+    if (!phone) {
+      return res.status(400).json({ message: "Phone number is required" });
+    };
+    let otpEntry = await OtpModel.findOne({ phone });
+
+    if (otpEntry) {
+      return res.status(429).json({ message: "Please wait before requesting a new OTP" });
+    }
+    const newotpValue = generateOtp();
+
+    otpEntry = new OtpModel({
+      phone,
+      otp: newotpValue,
+    });
+
+    await otpEntry.save();
+    await sendOtp(phone, newotpValue);
+    res.json({ message: "OTP sent successfully" });
+  } catch (error) {
+    console.log("Error occured:", error);
+    res.status(500).json("Error while resending OTP");
   }
 };
 
@@ -153,4 +163,5 @@ module.exports = {
   deleteUser,
   requestOtp,
   loginWithOtp,
+  resendOtp
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const jsonParser = express.json();
+const {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUserByUid,
+  loginWithOtp,
+  requestOtp,
+  updateUser,
+} = require("../controllers/userController");
 
-// Create User
-router.post('/users', userController.createUser);
+// Create User, Get All Users
+router.route("/users").post(createUser).get(getAllUsers)
 
-// Get All Users
-router.get('/users', userController.getAllUsers);
-
-// Get Single User by UID
-router.get('/users/:uid', userController.getUserByUid);
-
-// Update User
-router.put('/users/:uid', userController.updateUser);
-
-// Delete User
-router.delete('/users/:uid', userController.deleteUser);
+// Get Single User by UID, Update User, Delete User
+router.route("/users/:uid").get(getUserByUid).put(updateUser).delete(deleteUser)
 
 // Request OTP for login
-router.post('/request_otp', jsonParser, userController.requestOtp);
+router.post("/request_otp", requestOtp);
+
+//Resend OTP 
+router.post("/resend_otp", resendOtp);
 
 // Login with OTP
-router.post('/login_with_otp', jsonParser, userController.loginWithOtp);
+router.post("/login_with_otp", loginWithOtp);
 
 module.exports = router;
