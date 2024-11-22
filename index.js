f193d0f05e817a720a28eeeb4b105a16138e38c6
Rithick <rithick.panoor574@gmail.com>
2024-11-22 20:21:33 +0530

    fix:routing issues

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const io = socketIo(server, {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use(logger("dev")); 
+app.use(logger("dev"));
 
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
@@ -55,6 +55,11 @@ app.use("/api/status", statusRoutes);
 
 // Public Api for accessing images
 app.use("/api/img", express.static(__dirname + "/public/"));
+
+//not found handler
+app.use("*", (req, res, next) => {
+  res.status(404).json({ message: "API not found" });
+});
 
 // Error handling
 app.use((err, req, res, next) => {
--- routes/statuesRoutes.js
+++ routes/statuesRoutes.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const statusController = require('../controllers/statusController');
+const {
+  createStatus,
+  deleteStatus,
+  getAllStatuses,
+  getStatusById,
+  updateStatus,
+} = require("../controllers/statusController");
 
 // Route to create a new status
-router.post('/status', statusController.createStatus);
+router.post("/status", createStatus);
 
 // Route to get all statuses
-router.get('/statuses', statusController.getAllStatuses);
+router.get("/statuses", getAllStatuses);
 
 // Route to get a status by ID
-router.get('/status/:id', statusController.getStatusById);
+router.get("/status/:id", getStatusById);
 
 // Route to update a status by ID
-router.put('/status/:id', statusController.updateStatus);
+router.put("/status/:id", updateStatus);
 
 // Route to delete a status by ID
-router.delete('/status/:id', statusController.deleteStatus);
+router.delete("/status/:id", deleteStatus);
 
 module.exports = router;
